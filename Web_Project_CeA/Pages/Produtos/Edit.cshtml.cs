a01cc35dd241e7d06981c4f13d4d26ce64f8c9c9
Bruno Santana Lopes <85855111+beomont@users.noreply.github.com>
2021-10-27 11:43:23 -0300

    Correção função OnGetAsync

--- Web_Project_CeA/Pages/Produtos/Edit.cshtml.cs
+++ Web_Project_CeA/Pages/Produtos/Edit.cshtml.cs
@@ -16,7 +16,7 @@ namespace Web_Project_CeA.Pages.Produtos
         [BindProperty]
         public Produto Produto { get; set; }
         string baseUrl = "https://localhost:44329/";
-        public async Task<IActionResult> OnGetAsyn(int? id)
+        public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
             {

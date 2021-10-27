dc8a80cc8d8b55504e215b86293c5b60f3a72e5b
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-10-27 16:34:51 -0300

    Alteração baseUrl pagina editar produto

--- Web_Project_CeA/Pages/Produtos/Edit.cshtml.cs
+++ Web_Project_CeA/Pages/Produtos/Edit.cshtml.cs
@@ -15,7 +15,7 @@ namespace Web_Project_CeA.Pages.Produtos
     {
         [BindProperty]
         public Produto Produto { get; set; }
-        string baseUrl = "https://localhost:44343/";
+        string baseUrl = "https://localhost:44336/";
         public async Task<IActionResult> OnGetAsyn(int? id)
         {
             if (id == null)

f6814ac3842f938b9d3effd9185490b6a7fb4483
bazithiago <thiagovieira.dev@gmail.com>
2021-11-02 16:46:11 -0300

    Update getUser.js

--- Web_Project_CeA/wwwroot/js/getUser.js
+++ Web_Project_CeA/wwwroot/js/getUser.js
@@ -1,1 +1,6 @@
 document.getElementById('userName').innerHTML = localStorage.user;
+
+const logoff = document.getElementById('logoff');
+logoff.addEventListener('click', function() {
+    localStorage.clear();
+});

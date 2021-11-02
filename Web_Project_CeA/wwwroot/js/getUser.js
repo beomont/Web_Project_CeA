ccdc2647dd8396a9d7c2d8f6cdd279c88c2219f3
bazithiago <thiagovieira.dev@gmail.com>
2021-11-02 17:00:28 -0300

    Ajuste layout Edit + Update getUser.js

--- Web_Project_CeA/wwwroot/js/getUser.js
+++ Web_Project_CeA/wwwroot/js/getUser.js
@@ -1,4 +1,11 @@
+const userName = document.getElementById('userName');
+
+console.log(localStorage.user);
+if (localStorage.user == undefined) {
+    localStorage.user = 'Usuário anônimo';
+}
 document.getElementById('userName').innerHTML = localStorage.user;
+
 
 const logoff = document.getElementById('logoff');
 logoff.addEventListener('click', function() {
